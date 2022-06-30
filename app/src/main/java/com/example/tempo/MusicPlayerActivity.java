b44c4eedb96c62f97288ef2f67d9b3258e9765fa
zerodayz77 <charisyt26@gmail.com>
2022-06-30 18:34:32 -0400

    added new branch, along with a new activity window

---
+++ app/src/main/java/com/example/tempo/MusicPlayerActivity.java
@@ -0,0 +1,14 @@
+package com.example.tempo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_player);
+    }
+}
